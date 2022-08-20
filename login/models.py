c59931ab593f0a21411cdedf0bf2538800cd658d
Ariadny <ariadny.santos@escolar.ifrn.edu.br>
2022-08-20 15:36:09 -0300

    excluindo apps criadas

--- login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class login(models.Model):
-    login = models.CharField(max_length=50)
-    senha = models.CharField(max_length=50)
-
-    
